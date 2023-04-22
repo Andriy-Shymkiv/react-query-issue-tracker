9f3998e3fcdf0c04b74082ac89a72ddf6316e55b
Andriy Shymkiv <winamondezik@gmail.com>
2023-04-22 13:02:03 +0300

    then => await

--- src/hooks/useIssuesList.ts
+++ src/hooks/useIssuesList.ts
@@ -2,7 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import { Issue } from '../types/Issues';
 
 export const useIssuesList = () => {
-  const fetchIssues = () => fetch('/api/issues').then((res) => res.json());
+  const fetchIssues = async () => {
+    const response = await fetch('/api/issues');
+
+    return await response.json();
+  };
 
   return useQuery<Issue[]>(['issues'], fetchIssues);
 };
